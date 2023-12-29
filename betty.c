028170881475b5f632565f6f11db8f37cc888a17
Cianick <ccianickblack@gmail.com>
2023-12-29 03:28:51 -0800

    betty proud task

--- betty.c
+++ betty.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    int i = 42;  // Declare and initialize a variable
+/* This function prints a simple message to demonstrate functionality */
+int main(void)
+{
+    int i = 42;
 
-    /* Print a simple message to demonstrate functionality */
     printf("The value of i is: %d\n", i);
 
-    return 0;  // Indicate successful program termination
+    return (0);  // Indicate successful program termination
 }
 
