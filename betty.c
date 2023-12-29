e8baa236247f175738759b39536c0b6e6ec6e6a8
Cianick <ccianickblack@gmail.com>
2023-12-29 03:22:25 -0800

    betty proud task

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 42;  // Declare and initialize a variable
+
+    /* Print a simple message to demonstrate functionality */
+    printf("The value of i is: %d\n", i);
+
+    return 0;  // Indicate successful program termination
+}
+
